f6f3c37fad04eb1d00d49cb0c4118134243d75b4
mayagord <mayagord@umich.edu>
2025-04-09 14:38:22 -0400

    edits

--- maya.py
+++ maya.py
@@ -3,44 +3,38 @@ import requests
 import sqlite3
 import json
 
-# SCRAPING POPULATION DATA 
+def get_populations(): #Gets Population for each state 
+    url = "https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population"
 
-url = "https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-r = requests.get(url)
-soup = BeautifulSoup(r.content, 'html.parser')
+    table = soup.find('table', class_='sortable wikitable sticky-header-multi static-row-numbers sort-under col1left col2center') 
 
-table = soup.find('table', class_='sortable wikitable sticky-header-multi static-row-numbers sort-under col1left col2center') 
+    rows = table.find_all('tr')
 
-rows = table.find_all('tr')
+    city_data = {}
 
-city_data = {}
-
-for row in rows[1:]: 
-    cols = row.find_all('td')
-    if len(cols) >= 4:
-        city_tag = cols[0].find('a')
-        city = city_tag.get('title').strip()
-        state = cols[1].text.strip()
-        population = int(cols[3].text.strip().replace(',', ''))
-        longlat_full = cols[9].text.strip().split(' / ')
-        cords = longlat_full[-1].split()
-        long = float(cords[0].replace(';', ''))
-        lat = float(cords[1].replace('\ufeff', ''))
+    for row in rows[1:]: 
+        cols = row.find_all('td')
+        if len(cols) >= 4: #used ChatGPT
+            city_tag = cols[0].find('a')
+            city = city_tag.get('title').strip().split(',')[0]
+            state = cols[1].text.strip()
+            population = int(cols[3].text.strip().replace(',', ''))
+            longlat_full = cols[9].text.strip().split(' / ')
+            cords = longlat_full[-1].split()
+            long = float(cords[0].replace(';', ''))
+            lat = float(cords[1].replace('\ufeff', ''))
         
-        city_data[city] = {
-            "population": population,
-            "coordinates": {
-                "longitude": long,
-                "latitude": lat
+            city_data[f"{city}, {state}"] = {
+                "population": population,
+                "coordinates": {
+                    "longitude": long,
+                    "latitude": lat
             }
         }
 
-with open("city_info.json", "w") as f:
-    json.dump(city_data, f, indent=4)
+    print(city_data)
 
-print(json.dumps(city_data, indent=4))
-
-# IMPORTING DATA INTO TABLE
-
-
+get_populations()
